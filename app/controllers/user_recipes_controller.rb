76ecf0f5fe7c80124f406c947601be1fa119de87
aubreeabril <aubree.abril@gmail.com>
2018-10-01 14:27:07 -0400

    added note column to user recipes

--- app/controllers/user_recipes_controller.rb
+++ app/controllers/user_recipes_controller.rb
@@ -1,5 +1,5 @@
 class UserRecipesController < ApplicationController
-
+  before_action :find_user_recipe, only: [:destroy, :update]
   def index
     @user_recipes = UserRecipe.all
     render json: @user_recipes
@@ -10,9 +10,22 @@ class UserRecipesController < ApplicationController
     render json: @user_recipe
   end
 
+  def destroy
+    render json: @user_recipe.destroy
+  end
+
+  def update
+    @user_recipe.update(user_recipe_params)
+    render json: @user_recipe
+  end
+
   private
 
   def user_recipe_params
-    params.require(:user_recipe).permit(:user_id, :recipe_id)
+    params.require(:user_recipe).permit(:user_id, :recipe_id, :note)
+  end
+
+  def find_user_recipe
+    @user_recipe = UserRecipe.find(params[:id])
   end
 end
